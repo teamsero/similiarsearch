d986160bf00779164486c2219bc705f87fc5d1f1
david <david.tr.se@gmail.com>
2023-09-19 20:34:37 +0700

    update

--- README.md
+++ README.md
@@ -2,18 +2,22 @@
 ```
 $ make db/up
 ```
+
 2. Install python lib
 ```
 $ make init
 ```
+
 3. Make generate mock job:
 ```
 $ make seek
 ```
+
 4. Run v1 - model : all-MiniLM-L6-v2
 ```
 $ make run
 ```
+
 5. Run v2 - model: sentence-transformers/all-mpnet-base-v2
 ```
 $ make run2
--- qdrant_v2.py
+++ qdrant_v2.py
@@ -2,16 +2,17 @@ from qdrant_client import models, QdrantClient
 from sentence_transformers import SentenceTransformer
 import json
 
-encoder = SentenceTransformer('sentence-transformers/all-mpnet-base-v2') # Model to create embeddings
+# Model to create embeddings
+encoder = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
 
 client = QdrantClient("localhost", port=6333)
-# Let's make a semantic search for Sci-Fi books! 
+# Let's make a semantic search for Sci-Fi books!
 
 
 # Open the JSON file
 with open('seek.json') as file:
     # Load the JSON data
-     # Read the file contents
+    # Read the file contents
     file_contents = file.read()
 
     # Convert the file contents to JSON
@@ -21,7 +22,8 @@ with open('seek.json') as file:
 client.recreate_collection(
     collection_name="my_books2",
     vectors_config=models.VectorParams(
-        size=encoder.get_sentence_embedding_dimension(), # Vector size is defined by used model
+        # Vector size is defined by used model
+        size=encoder.get_sentence_embedding_dimension(),
         distance=models.Distance.COSINE
     )
 )
@@ -33,14 +35,14 @@ client.upload_records(
         models.Record(
             id=idx,
             # vector=encoder.encode(doc["title"] + " "+doc["desc"]).tolist(),
-            vector=encoder.encode(doc["title"] ).tolist(),
+            vector=encoder.encode(doc["title"]).tolist(),
             payload=doc
         ) for idx, doc in enumerate(documents)
     ]
 )
 
-keyword =  "chăm sóc" 
-print("keyword: ",keyword)
+keyword = "mentor"
+print("keyword: ", keyword)
 
 hits = client.search(
     collection_name="my_books2",
@@ -54,9 +56,16 @@ hits = client.search(
                     gte=2
                 )
             )
+        ],
+        must=[
+            models.FieldCondition(
+                key="reputation_level",
+                match=models.Match(
+                    any=["mentor"]
+                )
+            )
         ]
     ),
 )
 for hit in hits:
-  print(hit.payload, "score:", hit.score)
-
+    print(hit.payload, "score:", hit.score)
